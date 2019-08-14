3c68fc0438cfef0e2ddeb0e0142d08fac3ea740b
Alvin10919058 <zero840108@gmail.com>
2019-08-14 19:34:57 +0800

    feat: init phaser.js

--- src/router.js
+++ src/router.js
@@ -1,26 +1,32 @@
-import Vue from "vue";
-import Router from "vue-router";
-import MP3player from "./views/mp3-player";
-import OnlinePayment from "./views/online-payment";
+import Vue from 'vue'
+import Router from 'vue-router'
+import MP3player from './views/mp3-player'
+import OnlinePayment from './views/online-payment'
+import Game from './views/90s-game'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  routes: [
-    // {
-    //   path: "/",
-    //   name: "home",
-    //   component:
-    // },
-    {
-      path: "/week3",
-      name: "week3",
-      component: MP3player
-    },
-    {
-      path: "/week4",
-      name: "week4",
-      component: OnlinePayment
-    }
-  ]
-});
+    routes: [
+        // {
+        //   path: "/",
+        //   name: "home",
+        //   component:
+        // },
+        {
+            path: '/week3',
+            name: 'week3',
+            component: MP3player
+        },
+        {
+            path: '/week4',
+            name: 'week4',
+            component: OnlinePayment
+        },
+        {
+            path: '/week5',
+            name: 'week5',
+            component: Game
+        }
+    ]
+})
---
+++ src/views/90s-game/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="game-app"></div>
+</template>
+
+<script>
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 450,
+    parent: 'game-app',
+    scene: []
+}
+
+const game = new Phaser.Game(config)
+</script>
+
+<style lang="scss">
+#game-app {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    canvas {
+        width: 800px;
+        height: 450px;
+    }
+}
+</style>
