8f35bc33d951d559fd1dd768d32e7caebae3d012
Alvin10919058 <zero840108@gmail.com>
2019-08-15 08:17:32 +0800

    feat: add animate

--- src/views/90s-game/index.vue
+++ src/views/90s-game/index.vue
@@ -3,12 +3,53 @@
 </template>
 
 <script>
+const w = 800
+const h = 450
+const gameStart = {
+    key: 'gameStart',
+    preload: function() {
+        this.load.image('bg4', './assets/images/bg/bg4.png')
+        this.load.image('bg3', './assets/images/bg/bg3.png')
+        this.load.image('bg2', './assets/images/bg/bg2.png')
+        this.load.image('bg1', './assets/images/bg/bg1.png')
+        this.load.image('ground', './assets/images/bg/footer.png')
+        this.load.spritesheet('player', './assets/images/player.png', {
+            frameWidth: 144,
+            frameHeight: 120
+        })
+    },
+    create: function() {
+        this.bg4 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg4')
+        this.bg3 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg3')
+        this.bg2 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg2')
+        this.bg1 = this.add.tileSprite(w / 2, h / 2, w, h, 'bg1')
+        this.ground = this.add.tileSprite(w / 2, h - 45, w, 90, 'ground')
+
+        this.player = this.add.sprite(150, 150, 'player')
+        this.player.setScale(0.7)
+
+        this.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
+            frameRate: 5,
+            repeat: -1
+        })
+
+        this.player.anims.play('run', true)
+    },
+    update: function() {
+        this.bg3.tilePositionX += 2
+        this.bg2.tilePositionX += 3
+        this.bg1.tilePositionX += 4
+        this.ground.tilePositionX += 4
+    }
+}
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 450,
+    width: w,
+    height: h,
     parent: 'game-app',
-    scene: []
+    scene: [gameStart]
 }
 
 const game = new Phaser.Game(config)
